37581b1ca9ce7ad5ec82e87f403558537c0c162a
Simon MGK <contact@mgkprod.fr>
2019-05-06 22:39:27 +0200

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     <link rel="apple-touch-icon-precomposed" sizes="152x152" href="apple-touch-icon-152x152.png" />
     <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
     <link rel="icon" type="image/png" href="favicon-16x16.png" sizes="16x16" />
-    <meta name="application-name" content="&nbsp;"/>
+    <meta name="application-name" content="4sucres.org"/>
     <meta name="msapplication-TileColor" content="#213345" />
     <meta name="msapplication-TileImage" content="mstile-144x144.png" />
 
