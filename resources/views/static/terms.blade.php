9a629eed64582c7ceeda078374802081af71fb77
Simon MGK <contact@mgkprod.fr>
2019-05-07 01:30:04 +0200

    wip

--- resources/views/static/terms.blade.php
+++ resources/views/static/terms.blade.php
@@ -1,8 +1,18 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
-    // TODO
+    <div class="card">
+        <div class="card-body">
+            @php
+                echo Cache::remember('terms_md', now()->addHour(1), function () {
+                    $content = File::get(base_path('resources/views/static/markdown/terms.md'));
+                    return (new Parsedown())->text($content);
+                });
+            @endphp
+        </div>
+    </div>
 </div>
 
 @endsection
