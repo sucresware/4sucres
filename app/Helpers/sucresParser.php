9a5a7cfca1feb119afb91bb0201ae4405dcab154
Simon MGK <contact@mgkprod.fr>
2019-05-11 16:04:29 +0200

    wip

---
+++ app/Helpers/sucresParser.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace App\Helpers;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Support\Str;
+
+class sucresParser
+{
+    public $content;
+    protected $bbcode_parser;
+    protected $lightweight;
+    protected $protections;
+
+    public function __construct($content, $lightweight = false)
+    {
+        $this->content = $content;
+        $this->lightweight = $lightweight;
+        $this->parser = new \ChrisKonnertz\BBCode\BBCode();
+        $this->protections = [];
+    }
+
+    public function render(){
+        $this->addBBCodesToParser()
+             ->renderMock()
+             ->protectAttr('youtube')
+             ->protectAttr('url')
+             ->renderBB()
+             ->renderUrl()
+             ->renderYoutube()
+             ->renderMentions();
+
+        if (!$this->lightweight) {
+            $this->renderQuotes();
+        }
+
+        return $this->content;
+    }
+
+    public function addBBCodesToParser(){
+        $this->parser->addTag('glitch', function ($tag, &$html, $openingTag) {
+            if ($tag->opening) {
+                return '<span class="baffle">';
+            } else {
+                return '</span>';
+            }
+        });
+
+        return $this;
+    }
+
+    public function renderBB(){
+        $this->content = $this->parser->render($this->content);
+
+        return $this;
+    }
+
+    public function renderMock(){
+        $preg_result = [];
+        $regexp = '/(?:\[mock\])(.*?)(?:\[\/mock\])/';
+        preg_match_all($regexp, $this->content, $preg_result);
+
+        foreach ($preg_result[0] as $k => $tag) {
+            $str = str_split(strtolower($preg_result[1][$k]));
+            foreach ($str as &$char) {
+                if (rand(0, 1)) {
+                    $char = strtoupper($char);
+                }
+            }
+
+            $this->content = str_replace(
+                $tag, implode('', $str), $this->content
+            );
+        }
+
+        return $this;
+    }
+
+    public function renderUrl(){
+        $preg_result = [];
+        $regexp = '/{' . $this->protections['url'] . '(=.*?|)}(.*?){\/' . $this->protections['url'] . '}/';
+        preg_match_all($regexp, $this->content, $preg_result);
+
+        foreach ($preg_result[0] as $k => $match) {
+            $url = $preg_result[1][$k] == '' ? $preg_result[2][$k] : $preg_result[1][$k];
+            $url = trim($url, '=');
+            if ($preg_result[1][$k] != '' && $url != $preg_result[2][$k]) {
+                $preview = '<i class="fas fa-exclamation-triangle text-warning mr-1"></i> ' . $url;
+            } else {
+                $preview = '<i class="fas fa-check-circle text-success mr-1"></i> ' . $url;
+            }
+            $markup = "<a target='_blank' href='$url' data-toggle='tooltip' data-placement='top' data-html='true' title='$preview'>" . $preg_result[2][$k] . '</a>';
+            $this->content = str_replace($preg_result[0][$k], $markup, $this->content);
+        }
+
+        return $this;
+    }
+
+    public function renderYoutube(){
+        $preg_result = [];
+        $regexp = '/{' . $this->protections['youtube'] . '(=.*?|)}(.*?){\/' . $this->protections['youtube'] . '}/';
+        preg_match_all($regexp, $this->content, $preg_result);
+
+        foreach ($preg_result[0] as $k => $match) {
+            $youtube_id = $base_youtube_url = $preg_result[2][$k];
+            $youtube_id = str_replace('https://www.youtube.com/watch?v=', '', $youtube_id);
+            $youtube_id = str_replace('https://youtube.com/watch?v=', '', $youtube_id);
+            $youtube_id = str_replace('https://youtu.be/', '', $youtube_id);
+            $youtube_id = str_replace('https://www.youtube.com/embed/', '', $youtube_id);
+            $youtube_id = str_replace('http://www.youtube.com/watch?v=', '', $youtube_id);
+            $youtube_id = str_replace('http://youtube.com/watch?v=', '', $youtube_id);
+            $youtube_id = str_replace('http://youtu.be/', '', $youtube_id);
+            $youtube_id = str_replace('http://www.youtube.com/embed/', '', $youtube_id);
+            $youtube_id = explode('&', $youtube_id)[0];
+
+            $markup  = '<div class="integration" style="max-width: 500px">';
+            $markup .= '<div class="embed-responsive embed-responsive-16by9" style="max-width: 500px">';
+            $markup .= '<iframe class="embed-responsive-item" src="https://www.youtube.com/embed/' . $youtube_id . '?rel=0" allowfullscreen></iframe>';
+            $markup .= '</div>';
+            $markup .= '<div class="integration-text"><i class="fab fa-youtube text-danger"></i> <a target="_blank" href="' . $base_youtube_url . '">Ouvrir dans YouTube</a></div>';
+            $markup .= '</div>';
+
+            $this->content = str_replace($preg_result[0][$k], $markup, $this->content);
+        }
+
+        return $this;
+    }
+
+    public function renderMentions(){
+        $preg_result = [];
+        $regex = '/(?:@|#u:)(?:\w+)(?:<br\/>|<br>|[\s]|$|\z)/';
+        preg_match_all($regex, $this->content, $preg_result);
+
+        foreach ($preg_result[0] as $tag) {
+            $tag = trim($tag);
+            $clear_tag = trim(str_replace(['@', '#u:'], '', $tag));
+
+            $user = User::where('name', $clear_tag)->first();
+            if (!$user) { continue; }
+
+            $this->content = str_replace(
+                $tag, '<a href="' . $user->link . '" class="badge badge-primary">@' . $clear_tag . '</a>' . ' ', $this->content
+            );
+        }
+    }
+
+    public function renderQuotes(){
+        $preg_result = [];
+        $regex = '/(?:#p:)(?:\d+)(?:<br\/>|<br>|[\s]|$|\z)/';
+        preg_match_all($regex, $this->content, $preg_result);
+
+        foreach ($preg_result[0] as $tag) {
+            $tag = trim($tag);
+            $clear_tag = trim(str_replace(['#p:'], '', $tag));
+
+            $post = Post::find($clear_tag);
+            if (!$post || $post->discussion->private) {
+                continue;
+            }
+
+            $this->content = str_replace(
+                $tag, view('discussion.post._show_as_quote', compact('post'))->render(), $this->content
+            );
+        }
+    }
+
+    public function protectAttr($attr){
+        $attr_code = Str::uuid()->toString();
+
+        $regexp = '/\[' . $attr . '(=.*?|)\](.*?)\[\/' . $attr . '\]/';
+        $subst = '{' . $attr_code . '$1}$2{/' . $attr_code . '}';
+
+        $this->content = preg_replace($regexp, $subst, $this->content);
+        $this->protections[$attr] = $attr_code;
+
+        return $this;
+    }
+
+}
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Support\Str;
 use Qirolab\Laravel\Reactions\Contracts\ReactableInterface;
 use Qirolab\Laravel\Reactions\Traits\Reactable;
+use App\Helpers\sucresParser;
 
 class Post extends Model implements ReactableInterface
 {
@@ -98,131 +99,11 @@ class Post extends Model implements ReactableInterface
 
     public function getPresentedBodyAttribute()
     {
-        $body = $this->presented_light_body;
-
-        // Rendu des citations :
-        $preg_result = [];
-        preg_match_all('/(?:#p:)(?:\d+)(?:<br\/>|<br>|[\s]|$|\z)/', $body, $preg_result);
-
-        foreach ($preg_result[0] as $tag) {
-            $tag = trim($tag);
-            $clear_tag = trim(str_replace(['#p:'], '', $tag));
-
-            $post = Post::find($clear_tag);
-            if (!$post || $post->discussion->private) {
-                continue;
-            }
-
-            $body = str_replace(
-                $tag,
-                view('discussion.post._show_as_quote', compact('post'))->render(),
-                $body
-            );
-        }
-
-        return $body;
+        return (new sucresParser($this->body))->render();
     }
 
     public function getPresentedLightBodyAttribute()
     {
-        $body = $this->body;
-
-        // Rendu de [mock][/mock]
-        $preg_result = [];
-        preg_match_all('/(?:\[mock\])(.*?)(?:\[\/mock\])/', $body, $preg_result);
-
-        foreach ($preg_result[0] as $k => $tag) {
-            $str = str_split(strtolower($preg_result[1][$k]));
-            foreach ($str as &$char) {
-                if (rand(0, 1)) {
-                    $char = strtoupper($char);
-                }
-            }
-
-            $body = str_replace(
-                $tag,
-                implode('', $str),
-                $body
-            );
-        }
-
-        // Rendu du BBCode :
-        $bbcode = new \ChrisKonnertz\BBCode\BBCode();
-
-        $bbcode->addTag('glitch', function ($tag, &$html, $openingTag) {
-            if ($tag->opening) {
-                return '<span class="baffle">';
-            } else {
-                return '</span>';
-            }
-        });
-
-        // Prepare YouTube tag content
-        $re = '/\[youtube](.*?)\[\/youtube]/m';
-        $match = [];
-        preg_match($re, $body, $match);
-        if ($match[0] ?? null) {
-            $markup = '[youtube]';
-            $base_youtube_url = $match[1];
-            $base_youtube_url = str_replace('https://www.youtube.com/watch?v=', '', $base_youtube_url);
-            $base_youtube_url = str_replace('https://youtube.com/watch?v=', '', $base_youtube_url);
-            $base_youtube_url = str_replace('https://youtu.be/', '', $base_youtube_url);
-            $base_youtube_url = str_replace('https://www.youtube.com/embed/', '', $base_youtube_url);
-            $base_youtube_url = str_replace('http://www.youtube.com/watch?v=', '', $base_youtube_url);
-            $base_youtube_url = str_replace('http://youtube.com/watch?v=', '', $base_youtube_url);
-            $base_youtube_url = str_replace('http://youtu.be/', '', $base_youtube_url);
-            $base_youtube_url = str_replace('http://www.youtube.com/embed/', '', $base_youtube_url);
-            $base_youtube_url = explode('&', $base_youtube_url)[0];
-            $markup .= $base_youtube_url;
-            $markup .= '[/youtube]';
-            $body = str_replace($match[0], $markup, $body);
-        }
-
-        $re = '/\[url(=.*?|)\](.*?)\[\/url\]/';
-        $url_int_code = Str::uuid();
-        $subst = '{' . $url_int_code . '$1}$2{/' . $url_int_code . '}';
-        $body = preg_replace($re, $subst, $body);
-        $bbcode->setYouTubeWidth(560);
-        $bbcode->setYouTubeHeight(315);
-        $body = $bbcode->render($body);
-
-        // Fix urls
-        $re = '/{' . $url_int_code . '(=.*?|)}(.*?){\/' . $url_int_code . '}/';
-        $preg_result = [];
-        preg_match_all($re, $body, $preg_result);
-
-        foreach ($preg_result[0] as $k => $match) {
-            $url = $preg_result[1][$k] == '' ? $preg_result[2][$k] : $preg_result[1][$k];
-            $url = trim($url, '=');
-            if ($preg_result[1][$k] != '' && $url != $preg_result[2][$k]) {
-                $preview = '<i class="fas fa-exclamation-triangle text-warning mr-1"></i> ' . $url;
-            } else {
-                $preview = '<i class="fas fa-check-circle text-success mr-1"></i> ' . $url;
-            }
-            $markup = "<a target='_blank' href='$url' data-toggle='tooltip' data-placement='top' data-html='true' title='$preview'>" . $preg_result[2][$k] . '</a>';
-            $body = str_replace($preg_result[0][$k], $markup, $body);
-        }
-
-        // Rendu des mentions :
-        $preg_result = [];
-        preg_match_all('/(?:@|#u:)(?:\w+)(?:<br\/>|<br>|[\s]|$|\z)/', $body, $preg_result);
-
-        foreach ($preg_result[0] as $tag) {
-            $tag = trim($tag);
-            $clear_tag = trim(str_replace(['@', '#u:'], '', $tag));
-
-            $user = User::where('name', $clear_tag)->first();
-            if (!$user) {
-                continue;
-            }
-
-            $body = str_replace(
-                $tag,
-                '<a href="' . $user->link . '" class="badge badge-primary">@' . $clear_tag . '</a>' . ' ',
-                $body
-            );
-        }
-
-        return $body;
+        return (new sucresParser($this->body, true))->render();
     }
 }
