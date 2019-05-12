821300fd2edc72ff4f616a5873fd172ba345075c
Simon MGK <contact@mgkprod.fr>
2019-05-12 10:56:49 +0200

    wip

---
+++ app/Http/Middleware/DebugBarMiddleware.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class DebugBarMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure                 $next
+     *
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check() && auth()->user()->can('debug mode enabled')) {
+            config('app.debug', true);
+            \Debugbar::enable();
+        }
+
+        return $next($request);
+    }
+}
--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -25,13 +25,8 @@ class AuthServiceProvider extends ServiceProvider
 
         Gate::define('viewWebSocketsDashboard', function ($user = null) {
             return in_array($user->email, [
-                'sr@mgk.dev'
+                'sr@mgk.dev',
             ]);
         });
-
-        // if (auth()->check() && auth()->user()->can('debug mode enabled')) {
-        //     config('app.debug', true);
-        //     \Debugbar::enable();
-        // }
     }
 }
