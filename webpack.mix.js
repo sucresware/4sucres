88695fb5f7a81649c36d16617d1878b5bc0a1a8e
Enzo Innocenzi <enzo.inno@gmail.com>
2019-05-17 15:18:21 +0200

    Updated Mix to copy resources/ to public/ at runtime

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,12 +14,20 @@ const mix = require('laravel-mix');
 mix
     .js('resources/js/app.js', 'public/js')
     .js('resources/js/service-worker.js', 'public/js')
-    .sass('resources/sass/4sucres.scss', 'public/css');
-// .sass('resources/sass/material.scss', 'public/css')
-// .sass('resources/sass/nord.scss', 'public/css');
+    .sass('resources/sass/4sucres.scss', 'public/css')
+    // .sass('resources/sass/material.scss', 'public/css')
+    // .sass('resources/sass/nord.scss', 'public/css');
 
-mix
+    // Copies reused resources
+    .copyDirectory('resources/img',      'public/img')
+    .copyDirectory('resources/audio',    'public/audio')
+    .copyDirectory('resources/svg',      'public/svg')
+    .copyDirectory('resources/vendor',   'public/vendor')
+
+    .copyDirectory('resources/public',   'public/')
+
     .options({
         processCssUrls: false
     })
-    .version();
+    .version()
+;
