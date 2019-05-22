cfc56dfdbfa5f1e3b61a4e1ad9a6c6befd5cfc5d
Simon MGK <contact@mgkprod.fr>
2019-05-22 23:57:19 +0200

    Run UsersSeeder only on local env

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,31 +13,26 @@ class UsersTableSeeder extends Seeder
     {
         Model::unguard();
 
-        User::firstOrCreate([
-            'email' => 'sr@mgk.dev',
-        ], [
-            'name'              => 'YvonEnbaver',
-            'display_name'      => 'YvonEnbaver',
-            'shown_role'        => 'L\'élite des sucres',
-            'password'          => \Hash::make('1234'),
-            'email_verified_at' => now(),
-        ])->assignRole('admin');
+        if (config('app.env') === 'local') {
+            User::firstOrCreate([
+                'email' => 'sr@mgk.dev',
+            ], [
+                'name'              => 'YvonEnbaver',
+                'display_name'      => 'YvonEnbaver',
+                'shown_role'        => 'L\'élite des sucres',
+                'password'          => \Hash::make('1234'),
+                'email_verified_at' => now(),
+            ])->assignRole('admin');
 
-        User::firstOrCreate([
-            'email' => 'lorem@ipsum.dev',
-        ], [
-            'name'              => 'Hawezo',
-            'display_name'      => 'Hawezo',
-            'shown_role'        => 'gbesoindundeuxiemecompte',
-            'password'          => \Hash::make('1234'),
-            'email_verified_at' => now(),
-        ])->assignRole('admin');
-
-        // User::firstOrCreate([
-        //     'email' => '',
-        // ], [
-        //     'name' => 'Inspecteur_Olivier',
-        //     'password' => \Hash::make('1234'),
-        // ]);
+            User::firstOrCreate([
+                'email' => 'lorem@ipsum.dev',
+            ], [
+                'name'              => 'Hawezo',
+                'display_name'      => 'Hawezo',
+                'shown_role'        => 'gbesoindundeuxiemecompte',
+                'password'          => \Hash::make('1234'),
+                'email_verified_at' => now(),
+            ])->assignRole('admin');
+        }
     }
 }
