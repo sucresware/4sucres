6684baaab542621134f6536e06ee5d1066428e89
Simon MGK <contact@mgkprod.fr>
2019-06-01 12:48:31 +0200

    Add logic for private discussion notifications

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -124,12 +124,14 @@ class Discussion extends Model
     public function notify_subscibers(Post $post)
     {
         foreach ($this->subscribed as $user) {
-            if ($user->id != $post->user->id && $user->getSetting('notifications.on_subscribed_discussions', true)) {
-                // Check if the user has not already received an unread ReplyInDiscussion about this discussion :
-                $notifications = $user->notifications()
+            if ($user->id != $post->user->id) {
+                if ((!$post->discussion->private && $user->getSetting('notifications.on_subscribed_discussions', true)) || ($post->discussion->private && $user->getSetting('notifications.on_new_private_message', true))) {
+                    // Check if the user has not already received an unread ReplyInDiscussion about this discussion :
+                    $notifications = $user->notifications()
                     ->where('data->discussion_id', $post->discussion->id)
                     ->where('read_at', null)
                     ->whereIn('type', [ReplyInDiscussion::class, RepliesInDiscussion::class]);
+                }
 
                 if ($notifications->count()) {
                     $notifications->update(['read_at' => now()]);
