05c7387c78491ebd6b278fe7d3b364672791e838
Simon MGK <contact@mgkprod.fr>
2019-06-03 19:53:02 +0200

    Disabled Adsense

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,13 +44,13 @@
             gtag('config', 'UA-139755516-1');
         </script>
 
-        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        {{--  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
         <script>
             (adsbygoogle = window.adsbygoogle || []).push({
                 google_ad_client: "ca-pub-1896835277768477",
                 enable_page_level_ads: true
             });
-        </script>
+        </script>  --}}
     @endif
 
     @stack('css')
