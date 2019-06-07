a6e654628a0bb54cd94107bc959ff3322fd59967
Simon MGK <contact@mgkprod.fr>
2019-06-07 18:32:02 +0200

    Added footer ads

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,8 +45,8 @@
             gtag('config', 'UA-139755516-1');
         </script>
 
-        {{--  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
-        <script>
+        <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+        {{--  <script>
             (adsbygoogle = window.adsbygoogle || []).push({
                 google_ad_client: "ca-pub-1896835277768477",
                 enable_page_level_ads: true
@@ -231,6 +231,20 @@
         </main>
 
         <footer>
+            <hr>
+
+            <!-- Footer -->
+            <ins class="adsbygoogle"
+                style="display:block"
+                data-ad-client="ca-pub-1896835277768477"
+                data-ad-slot="4891883467"
+                data-ad-format="auto"
+                data-full-width-responsive="true"></ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </script>
+            <br><br>
+
             <img src="{{ url('/img/4sucres_alt_glitched.png') }}" width="70px">
             &copy; 2019<br>
             <br>
