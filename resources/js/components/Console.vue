deeaec0af66cc2b6ff6fbe35c527a7fb814fe24a
Simon MGK <contact@mgkprod.fr>
2019-07-07 19:56:47 +0200

    Added ctrl+shift+L to clear the console

--- resources/js/components/Console.vue
+++ resources/js/components/Console.vue
@@ -4,7 +4,13 @@
       <div v-for="line in lines" v-bind:key="line.index" v-html="line" class="output"></div>
     </div>
     <div class="prompt">
-      <input type="text" v-model="command" class="form-control" v-on:keyup.enter="run()" />
+      <input
+        type="text"
+        v-model="command"
+        class="form-control"
+        v-hotkey="keymap"
+        v-on:keyup.enter="run()"
+      />
       <div class="loading-indicator" v-show="loading">
         <i class="fas fa-circle-notch fa-spin"></i>
       </div>
@@ -27,11 +33,14 @@ export default {
   methods: {
     run(command) {
       let vm = this;
+      command = command ? command : this.command;
+
+      this.lines.push('<span class="text-muted">›</span> ' + command);
       this.loading = true;
 
-      AuthedAxios.get(command ? command : this.command)
+      AuthedAxios.get(command)
         .then(resp => {
-          vm.lines.push(resp.data.output);
+          vm.lines.push(resp.data.output + "<br><br>");
         })
         .catch(error => {
           vm.lines.push(error);
@@ -45,11 +54,23 @@ export default {
     scroll() {
       var content = this.$el.querySelector("#content");
       content.scrollTop = content.scrollHeight;
+    },
+    clear() {
+      this.lines = [];
+      var content = this.$el.querySelector("#content");
+      content.focus();
     }
   },
   mounted() {
     AuthedAxios.defaults.baseURL = "/admin/console/run/";
     this.run("help");
+  },
+  computed: {
+    keymap() {
+      return {
+        "ctrl+shift+l": this.clear
+      };
+    }
   }
 };
 </script>
