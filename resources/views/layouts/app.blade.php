000ef35653997ffb1e5acced7aff66e665204254
Simon MGK <contact@mgkprod.fr>
2019-08-22 10:38:32 +0200

    Add VocaBank link in the footer

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -219,6 +219,7 @@
             Temps d'exécution : {{ round((microtime(true) - LARAVEL_START), 3) }} s<br>
             <a href="{{ route('terms') }}">Conditions générales d'utilisation</a> <span class="mx-1">&mdash;</span>
             <a href="{{ route('charter') }}">Charte d'utilisation</a> <span class="mx-1">&mdash;</span>
+            <a href="https://vocabank.org" target="_blank">VocaBank</a><span class="mx-1">&mdash;</span>
             <a href="https://github.com/4sucres/board" target="_blank">GitHub</a>
         </footer>
     </div>
