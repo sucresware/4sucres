69c42cd63e62a4d66cdcf9d82b5e67d59ed4cc88
Enzo Innocenzi <enzo.inno@gmail.com>
2019-11-02 20:06:18 +0100

    Updated user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -17,11 +17,10 @@ use Faker\Generator as Faker;
 
 $factory->define(User::class, function (Faker $faker) {
     return [
-        'username'   => $faker->unique()->userName,
-        'first_name' => $faker->firstName,
-        'last_name'  => $faker->lastName,
-        'email'      => $faker->unique()->safeEmail,
-        'password'   => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-        'settings'   => [],
+        'username'     => $username = $faker->unique()->userName,
+        'display_name' => $username,
+        'email'        => $faker->unique()->safeEmail,
+        'password'     => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'settings'     => [],
     ];
 });
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,7 +5,7 @@ use Illuminate\Database\Seeder;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed the application's database.
+     * Seed the application's database in local environment.
      */
     public function run()
     {
