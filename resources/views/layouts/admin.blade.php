1bcfe34ae6de01f83fa06128c38539bbdf25ac53
Edouard <edou68@gmail.com>
2020-06-14 22:44:35 +0200

    Fix theme not workin on admin interface

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -19,8 +19,16 @@
     </title>
 
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+    
     <link href="{{ mix('css/theme-light.css') }}" rel="stylesheet" id="lightTheme">
-    <link href="{{ mix('css/theme-dark.css') }}" rel="stylesheet" id="darkTheme" @if (auth()->check() && user()->getSetting('layout.theme', 'light-theme') != 'dark-theme') disabled @endif>
+    @if (auth()->check())
+        <link href="{{ mix('css/theme-dark.css') }}" rel="stylesheet" id="darkTheme" @if (user()->getSetting('layout.theme', 'light-theme') != 'dark-theme') disabled @endif>
+        @if (!in_array(user()->getSetting('layout.theme', 'light-theme'), ['light-theme', 'dark-theme']))
+            <link href="{{ mix('css/theme-onche-light.css') }}" rel="stylesheet" @if (user()->getSetting('layout.theme', 'light-theme') != 'onche-light-theme') disabled @endif>
+            <link href="{{ mix('css/theme-avn-light.css') }}" rel="stylesheet" @if (user()->getSetting('layout.theme', 'light-theme') != 'avn-light-theme') disabled @endif>
+            <link href="{{ mix('css/theme-synth.css') }}" rel="stylesheet" @if (user()->getSetting('layout.theme', 'light-theme') != 'synth-theme') disabled @endif>
+        @endif
+    @endif
 
     <link rel="icon" type="image/png" href="{{ url('/img/icons/favicon-32x32-admin.png') }}" sizes="32x32">
     <link rel="icon" type="image/png" href="{{ url('/img/icons/favicon-16x16-admin.png') }}" sizes="16x16">
@@ -55,13 +63,14 @@
                                 @endif
                             </span>
                         </a>
-
-                        <a class="text-center mr-1 order-lg-8" href="javascript:void(0)" data-action="light-toggle">
-                            <span class="fa-stack notification">
-                                <i class="fas fa-circle fa-stack-2x notification-background"></i>
-                                <i class="fas fa-lightbulb fa-stack-1x fa-sm notification-icon"></i>
-                            </span>
-                        </a>
+                        @if (in_array(user()->getSetting('layout.theme', 'light-theme'), ['light-theme', 'dark-theme']))
+                            <a class="text-center mr-1 order-lg-8" href="javascript:void(0)" data-action="light-toggle">
+                                <span class="fa-stack notification">
+                                    <i class="fas fa-circle fa-stack-2x notification-background"></i>
+                                    <i class="fas fa-lightbulb fa-stack-1x fa-sm notification-icon"></i>
+                                </span>
+                            </a>
+                        @endif
 
                         @if (user()->hasRole('admin') || user()->hasRole('moderator'))
                             <a class="text-center mr-1 order-lg-9" href="{{ route('admin.index') }}">
