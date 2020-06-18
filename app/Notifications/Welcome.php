9b4e1fd1f4076875d5e43a10125c5bda70e578d8
Edouard <edou68@gmail.com>
2020-06-18 18:07:24 +0200

    Add notification for new users

---
+++ app/Notifications/Welcome.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use App\Models\User;
+
+class Welcome extends DefaultNotification
+{
+    use Queueable;
+
+    public $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    protected function attributes()
+    {
+        $attributes = [
+            'title'  => 'Bienvenue sur 4sucres.org !',
+            'target' => route('user.settings.layout'),
+            'html'   => 'Bienvenue <b>' . $this->user->getDisplayNameAttribute() . '</b> , prends le temps de personnaliser ton interface dans les paramètres.',
+            'text'   => 'Bienvenue, prends le temps de personnaliser ton interface dans les paramètres.',
+        ];
+
+        return $attributes;
+    }
+}
